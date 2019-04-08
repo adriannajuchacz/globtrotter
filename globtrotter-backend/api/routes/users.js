b0db56034b528171bd7400c221efaba9dcd4881a
Adrianna <adrianna.juchacz@gmail.com>
2019-04-08 15:39:04 +0200

    added Node.js backend with a docker container

---
+++ globtrotter-backend/api/routes/users.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const User = require("../models/user");
+
+router.get("/", (req, res, next) => {
+  User.find()
+    .select("name score _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        users: docs.map(doc => {
+          return {
+            name: doc.name,
+            score: doc.score,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/users/" + doc._id
+            }
+          };
+        })
+      };
+      //   if (docs.length >= 0) {
+      res.status(200).json(response);
+      //   } else {
+      //       res.status(404).json({
+      //           message: 'No entries found'
+      //       });
+      //   }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.post("/", (req, res, next) => {
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    score: 0
+  });
+  user
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created user successfully",
+        createdUser: {
+            name: result.name,
+            score: result.score,
+            _id: result._id,
+            request: {
+                type: 'GET',
+                url: "http://localhost:3000/users/" + result._id
+            }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.get("/:userId", (req, res, next) => {
+  const id = req.params.userId;
+  User.findById(id)
+    .select('name score _id')
+    .exec()
+    .then(doc => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json({
+            user: doc,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/users'
+            }
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.patch("/:userId", (req, res, next) => {
+  const id = req.params.userId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  User.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+          message: 'User updated',
+          request: {
+              type: 'GET',
+              url: 'http://localhost:3000/users/' + id
+          }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.delete("/:userId", (req, res, next) => {
+  const id = req.params.userId;
+  User.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+          message: 'User deleted',
+          request: {
+              type: 'POST',
+              url: 'http://localhost:3000/users',
+              body: { name: 'String', score: 'Number' }
+          }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+module.exports = router;
