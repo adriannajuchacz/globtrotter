8303032ed3ef0242ff870b8d9b61f6ad7fe028aa
Adrianna Juchacz <adrianna.juchacz@gmail.com>
2020-01-23 10:09:50 +0100

    added amcharts 4

--- globtrotter-frontend/src/router/index.js
+++ globtrotter-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Europe from '@/components/Europe'
+import World from '@/components/World'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Europe',
       component: Europe
+    },
+    {
+      path: '/world',
+      name: 'World',
+      component: World
     }
   ]
 })
